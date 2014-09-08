54620e56d685f5acc9a8356ce08dc6bfbffd1653
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-09-08 00:55:03 -0400

    finished version of the site
    need to complete items listed in Readme

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  $("#nav-mobile").html($("#nav-main").html());
+  $("#nav-trigger span").click(function(){
+    if ($("nav#nav-mobile ul").hasClass("expanded")) {
+        $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+        $(this).removeClass("open");
+    } else {
+        $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+        $(this).addClass("open");
+    }
+  });
+  
+});
